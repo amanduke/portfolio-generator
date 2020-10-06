8f8e1dce0b71368497be708fb3343fc6d81c7bf4
amanduke <65379991+amanduke@users.noreply.github.com>
2020-10-06 18:59:43 -0400

    feature capture-input

--- app.js
+++ app.js
@@ -1,1 +1,39 @@
-console.log('Hello Node');
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+const printProfileData = (profileDataArr) => {
+    //This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+        console.log(profileDataArr[i]);
+    }
+
+    console.log('===============');
+
+    // Is the same as this...
+    profileDataArr.forEach(profileItem => console.log(profileItem)); {
+        console.log(profileItem)
+    };
+};
+
+// printProfileData(profileDataArgs);
+
+// const message = 'Hello Node!';
+// message = 'Goodbye Node!';
+
+// const sum = 5 + 3;
+// sum += 1;
+
+
+
+// var commandLineArgs = process.argv;
+// console.log(commandLineArgs);
+// console.log('Hi');
+
+
+// var message = 'Hello Node!';
+
+// var sum = 5 + 3;
+
+// console.log(message);
+
+// console.log(sum);
