29bf7994240ac185e568d8adc1393f26d3049bf6
amanduke <65379991+amanduke@users.noreply.github.com>
2020-10-07 15:10:28 -0400

    questions

--- app.js
+++ app.js
@@ -2,82 +2,98 @@
 const inquirer = require('inquirer');
 console.log(inquirer)
 
-inquirer
-    .prompt([
+
+const promptUser = () => {
+    return inquirer.prompt([
         {
             type: 'input',
             name: 'name',
             message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your Github Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
         }
+    ]);
+};
+
+promptUser().then(answers => console.log(answers));
+
+const promptProject = portfolioData => {
+    console.log(`
+    =================
+    Add a New Project
+    =================
+    `);
+
+    // portfolioData.projects = [];
+    //if there's no 'projects' array property, create one
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
+    }
+
+    return inquirer
+    .prompt([
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is the name of your project?'
+      },
+      {
+        type: 'input',
+        name: 'description',
+        message: 'Provide a description of the project (Required)'
+      },
+      {
+        type: 'checkbox',
+        name: 'languages',
+        message: 'What did you build this project with? (Check all that apply)',
+        choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+      },
+      {
+        type: 'input',
+        name: 'link',
+        message: 'Enter the GitHub link to your project. (Required)'
+      },
+      {
+        type: 'confirm',
+        name: 'feature',
+        message: 'Would you like to feature this project?',
+        default: false
+      },
+      {
+        type: 'confirm',
+        name: 'confirmAddProject',
+        message: 'Would you like to enter another project?',
+        default: false
+      }
+  
     ])
-    .then(answers => console.log(answers));
-
-
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// // const { fstat } = require("fs/promises");
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('./index.html', generatePage(name, github), err => {
-//     if (err) throw err;
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+          return promptProject(portfolioData);
+        } else {
+          return portfolioData;
+        }
+      });
+    };
     
-//     console.log('Portfolio complete! Check out index.html to see the output!!');
-// });
+      promptUser()
+      .then(promptProject)
+      .then(portfolioData => {
+          console.log(portfolioData);
+      });
+ 
 
-
-
-// const generatePage = (userName, githubName) => {
-//     return `
-//     Name: ${userName}
-//     GitHub: ${githubName}
-//     `;
     
-// };
-
-
-// console.log('Jane', 'Janehub');
-// console.log(generatePage('Jane', 'janehub'));
-
-// const generatePage = (userName, githubName) => `Name: ${userName}, Github: ${githubName}`;
-// console.log(generatePage('Jane', 'janehub'));
 
 
 
-// const printProfileData = (profileDataArr) => {
-//     //This...
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-
-//     console.log('===============');
-
-//     // Is the same as this...
-//     profileDataArr.forEach(profileItem => console.log(profileItem)); {
-//         console.log(profileItem)
-//     };
-// };
-
-// printProfileData(profileDataArgs);
-
-// const message = 'Hello Node!';
-// message = 'Goodbye Node!';
-
-// const sum = 5 + 3;
-// sum += 1;
-
-
-
-// var commandLineArgs = process.argv;
-// console.log(commandLineArgs);
-// console.log('Hi');
-
-
-// var message = 'Hello Node!';
-
-// var sum = 5 + 3;
-
-// console.log(message);
-
-// console.log(sum);
+ 
